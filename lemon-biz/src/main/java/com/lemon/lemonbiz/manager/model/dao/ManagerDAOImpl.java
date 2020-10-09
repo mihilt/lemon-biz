21e195b35b4bb786de8aec949899bb770a6ab165
mihilt <kwschic@gmail.com>
2020-10-09 22:19:47 +0900

    feat : Add manageRank , insertDept
    Complete all of the implementations for managing rank

--- lemon-biz/src/main/java/com/lemon/lemonbiz/manager/model/dao/ManagerDAOImpl.java
+++ lemon-biz/src/main/java/com/lemon/lemonbiz/manager/model/dao/ManagerDAOImpl.java
@@ -1,14 +1,10 @@
 package com.lemon.lemonbiz.manager.model.dao;
 
-import java.util.List;
-import java.util.Map;
-
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.lemon.lemonbiz.member.model.vo.Dept;
-import com.lemon.lemonbiz.member.model.vo.Member;
 import com.lemon.lemonbiz.member.model.vo.Rank;
 
 @Repository
@@ -21,5 +17,31 @@ public class ManagerDAOImpl implements ManagerDAO{
 	public int insertRank(Rank rank) {
 		return sqlSession.insert("manager.insertRank", rank);
 	}
+
+	@Override
+	public int updateRank(Rank rank) {
+		return sqlSession.update("manager.updateRank", rank);
+	}
+
+	@Override
+	public int deleteRank(Rank rank) {
+		return sqlSession.delete("manager.deleteRank", rank);
+	}
+
+	@Override
+	public Dept selectOneDept(Dept dept) {
+		return sqlSession.selectOne("manager.selectOneDept", dept);
+	}
+
+	@Override
+	public Dept selectOneRefDept(Dept dept) {
+		return sqlSession.selectOne("manager.selectOneRefDept", dept);
+	}
+
+	@Override
+	public int insertDept(Dept dept) {
+		return sqlSession.insert("manager.insertDept", dept);
+	}
+	
 	
 }
